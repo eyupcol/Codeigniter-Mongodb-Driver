421b02ce4e36eefd8d4da91bc3505a7cb1144b0a
el-ma <eyupcol@yahoo.com>
2016-05-25 11:42:23 +0300

    Update Mdb.php

--- Mdb.php
+++ Mdb.php
@@ -2,23 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
-    Copyright (C) <2016>  <el-ma>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
  * Mongodb Codeigniter Driver v1.0
  * Versions: CI 3.X , Mongodb 3.2+ (also can be used with CI 2.X) , PHP 5 and PHP 7
  * Author: el-ma
