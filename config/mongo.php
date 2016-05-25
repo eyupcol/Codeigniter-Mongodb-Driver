e9e4bb353d1dbefb5a2ea17fc3e666e08b0fc60e
el-ma <eyupcol@yahoo.com>
2016-05-25 16:37:15 +0300

    Create mongo.php

---
+++ config/mongo.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$mongo['host'] = 'localhost';
+$mongo['port'] = '27017';
+$mongo['dbname'] = '';
+$mongo['user'] = '';
+$mongo['password'] = '';
+$config['mongo'] = $mongo;
