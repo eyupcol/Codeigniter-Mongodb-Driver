562e85b037cc1c7dc9f7fbf9242b4049c317b896
el-ma <eyupcol@yahoo.com>
2016-05-30 23:53:08 +0300

    Update mongo.php

--- config/mongo.php
+++ config/mongo.php
@@ -6,4 +6,5 @@ $mongo['port'] = '27017';
 $mongo['dbname'] = '';
 $mongo['user'] = '';
 $mongo['password'] = '';
+$mongo['connString'] = null; // if you need to custom connection string, just set this. Exp. mongodb://localhost:27017
 $config['mongo'] = $mongo;
