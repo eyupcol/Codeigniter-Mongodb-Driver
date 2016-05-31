9b3ab7d7f851e6464f490c31a15ab6ac794a342d
el-ma <eyupcol@yahoo.com>
2016-05-31 12:05:05 +0300

    Create mongo_lang.php

---
+++ language/english/mongo_lang.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+* https://github.com/el-ma/Codeigniter-Mongodb-Driver
+*/
+
+$lang['mongo_invalid_data'] = 'Invalid Data';
+$lang['mongo_connection_error'] = 'Connection Error';
+$lang['mongo_insert_error'] = 'Could not inserted';
+$lang['mongo_update_error'] = 'Could not updated';
+$lang['mongo_delete_error'] = 'Could not deleted';
+
+$lang['mongo_query_error'] = 'Query could not be executed';
+$lang['mongo_insert_batch_error'] = 'Data set could not inserted';
+$lang['mongo_invalid_query_filter'] = 'Query filter is invalid';
+$lang['mongo_invalid_query_options'] = 'Query options are invalid';
